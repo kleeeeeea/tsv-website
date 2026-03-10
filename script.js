caeaeafd73a82a1eeaf725d1321d6a7b89ac9172
Lea Kleemann <leakleemann@MacBook-Pro-Lea.local>
2026-03-10 22:03:45 +0100

    Add on-site TSV song player

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ if (navToggle && nav) {
     navToggle.setAttribute("aria-expanded", String(isOpen));
   });
 }
+
+const audioToggle = document.querySelector("[data-audio-toggle]");
+const clubAudio = document.querySelector("[data-club-audio]");
+
+if (audioToggle && clubAudio) {
+  const syncAudioState = () => {
+    audioToggle.classList.toggle("is-playing", !clubAudio.paused);
+  };
+
+  const tryPlayAudio = () => {
+    clubAudio.play().then(syncAudioState).catch(() => {
+      syncAudioState();
+    });
+  };
+
+  audioToggle.addEventListener("click", () => {
+    if (clubAudio.paused) {
+      tryPlayAudio();
+    } else {
+      clubAudio.pause();
+      syncAudioState();
+    }
+  });
+
+  clubAudio.addEventListener("play", syncAudioState);
+  clubAudio.addEventListener("pause", syncAudioState);
+
+  window.addEventListener("load", () => {
+    tryPlayAudio();
+  });
+}
