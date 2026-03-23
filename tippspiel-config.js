415c18603fd361dd257cee8075c7794796b26250
Lea Kleemann <leakleemann@MacBook-Pro-Lea.local>
2026-03-23 17:56:28 +0100

    Configure Supabase tipping game

--- tippspiel-config.js
+++ tippspiel-config.js
@@ -1,6 +1,6 @@
 window.tsvTippspielConfig = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://yarlxyfkzhlcfkyiwtzp.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlhcmx4eWZremhsY2ZreWl3dHpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNjc2MDcsImV4cCI6MjA4OTg0MzYwN30.i1EYDAFx19L8gJv5_EX1Qj0pjXrmbjEWhqTKOaHxLjc",
   seasonLabel: "2025/2026",
   clubName: "TSV Hainsfarth",
 };
